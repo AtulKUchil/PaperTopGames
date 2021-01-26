a4123b82fe7b674af0a368e4661cace215391cb6
AtulKUchil <atulkuchil@gmail.com>
2021-01-26 20:18:36 +0530

    added db and auth

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
-var createError = require('http-errors');
 var express = require('express');
+var ejs = require('ejs');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-// view engine setup
+mongoose.connect('mongodb://localhost/ManualAuth');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/client'));
 
-app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-app
-  .get( '*', function( req, res ) {
-    
-  });
+var index = require('./routes/index');
+app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+// define as the last app.use callback
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
-module.exports = app;
+
+// listen on port 3000
+app.listen(3000, () => {
+  console.log('Express app listening on port 3000');
+});
